85840878090cc78c68af7505a97b6da2e82ce382
NGx-A <98825795+NGx-A@users.noreply.github.com>
2022-02-09 19:47:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-let sound1 = document.querySelector(".sound1")
-let sound2 = document.querySelector(".sound2")
-let sound3 = document.querySelector(".sound3")
-let sound4 = document.querySelector(".sound4")
 let currentSound = 1
 let volume = 1
 
+let sound = [
+    document.querySelector(".sound1"), 
+    document.querySelector(".sound2"),
+    document.querySelector(".sound3"),
+    document.querySelector(".sound4"),
+    document.querySelector(".sound5"),
+    document.querySelector(".sound6"),
+    document.querySelector(".sound7")
+]
+
 document.querySelector(".button").addEventListener("click", e => {
-    sound1.volume = volume
-    sound2.volume = volume
-    sound3.volume = volume
-    sound4.volume = volume
-    if(currentSound == 1) sound1.play()
-    if(currentSound == 2) sound2.play()
-    if(currentSound == 3) sound3.play()
-    if(currentSound == 4) sound4.play()
+    for(i = 0; i < sound.length; i++) {
+        sound[i].volume = volume
+    }
+    sound[currentSound - 1].play()
 })
 
 document.addEventListener("keypress", Event => {
     //console.log(Event)
-    if(Event.key == "1") {
-        currentSound = 1
-        document.querySelector(".currentSound").textContent = "Lyd: Skratta ta"
+    if(Event.key == "a") {
+        currentSound -= 1
+        if(currentSound <= 0) currentSound = sound.length
+        document.querySelector(".currentSound").textContent = `Lyd: #${currentSound}`
     }
-    if(Event.key == "2") {
-        currentSound = 2
-        document.querySelector(".currentSound").textContent = "Lyd: The juice of lemons makes fine punch"
-    }
-    if(Event.key == "3") {
-        currentSound = 3
-        document.querySelector(".currentSound").textContent = "Lyd: Kreativ tekst"
-    }
-    if(Event.key == "4") {
-        currentSound = 4
-        document.querySelector(".currentSound").textContent = "Lyd: Så ellers går det bra"
+
+    if(Event.key == "d") {
+        currentSound += 1
+        if(currentSound >= sound.length + 1) currentSound = 1
+        document.querySelector(".currentSound").textContent = `Lyd: #${currentSound}`
     }
 
     if(Event.key == "w") {
@@ -40,17 +37,16 @@ document.addEventListener("keypress", Event => {
             volume += 0.1
             if(volume > 1) volume = 1
             document.querySelector(".volume").textContent = `Volum: ${(volume * 100).toFixed(0)}%`
+            document.querySelector(".volume").style.filter = "hue-rotate(" + volume * 300 + "deg)";
         }
-        
     }
     if(Event.key == "s") {
         if(volume > 0.1) {
             volume -= 0.1
             if(volume < 0) volume = 0
             document.querySelector(".volume").textContent = `Volum: ${(volume * 100).toFixed(0)}%`
+            document.querySelector(".volume").style.filter = "hue-rotate(" + volume * 300 + "deg)";
         }
-        
-        
     }
     
 })
